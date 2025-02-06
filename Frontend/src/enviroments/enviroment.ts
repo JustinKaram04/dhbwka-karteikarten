93e325c9f5c4b8d74967b40bb779917e3a15ae7a
DE\KAJ2BUE <justin.karam@de.bosch.com>
2025-02-06 09:20:46 +0100

    Neue Ordnerstruktur mit enviroment und authtoken

---
+++ Frontend/src/enviroments/enviroment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000/api', // URL für Backend 
+    auth: {
+      tokenStorageKey: 'auth_token', // Speicher-Schlüssel für  Token
+    },
+  }
