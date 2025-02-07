904b1cad126f407b8c16888969690155a5e39808
DE\KAJ2BUE <justin.karam@de.bosch.com>
2025-02-07 18:22:32 +0100

    Startseite mit Container für themenbereiche und sidebar

---
+++ Frontend/src/app/features/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
+import { CardComponent } from '../../shared/card/card.component';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule, SidebarComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+  topics = ['Politik', 'Physik', 'Geschichte'];
+}
