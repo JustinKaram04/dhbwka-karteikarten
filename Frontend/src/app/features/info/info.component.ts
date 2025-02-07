cb2af12af4127cdb74919d23d0dbad45cd48a481
DE\KAJ2BUE <justin.karam@de.bosch.com>
2025-02-07 21:29:26 +0100

    Css Verbessert, Infoseite hinzugefügt, provisorischer header, routen hinzugefügt

---
+++ Frontend/src/app/features/info/info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
+@Component({
+  selector: 'app-info',
+  standalone: true,
+  imports: [SidebarComponent],
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.css'
+})
+export class InfoComponent {
+
+}
