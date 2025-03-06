cfab01159b5c0337994f2a1c1e56fb3ca519e84f
DE\KAJ2BUE <justin.karam@de.bosch.com>
2025-03-06 16:25:53 +0100

    neu

--- Frontend/src/app/features/info/info.component.ts
+++ Frontend/src/app/features/info/info.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
 import { HeaderComponent } from "../../shared/components/header/header.component";
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [SidebarComponent, HeaderComponent],
+  imports: [HeaderComponent],
   templateUrl: './info.component.html',
   styleUrl: './info.component.css'
 })
